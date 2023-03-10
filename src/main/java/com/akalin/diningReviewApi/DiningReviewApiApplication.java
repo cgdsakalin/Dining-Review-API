150848bfd4826eaeb43a736d238d458ed311b53a
Selahattin Çağdaş AKALIN <cgdsakalin@gmail.com>
2023-03-10 16:39:05 +0300

    First Commit

---
+++ src/main/java/com/akalin/diningReviewApi/DiningReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.akalin.diningReviewApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewApiApplication.class, args);
+	}
+
+}
